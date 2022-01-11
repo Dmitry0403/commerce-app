f1ccf8e2c0c3c9a4e00eeb10d0e69ed0c7f18832
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2022-01-11 21:12:08 +0300

    установил webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  // мод в котором делается сборка. Для production применяются оптимизации, для development добавляется функционал для удобной разработки
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  // начальный файл проекта, с которого начинается сборка
+  entry: "./src/index.tsx",
+
+  module: {
+    // массив лоадеров, т.к. webpack умеет собирать только JS файлы,
+    // то нам нужны для каждого отличного от JS файла лоадер, который научит webpack работать с соответствующим расширением
+    rules: [
+      // лоадер, который учит работать с .ts и .tsx файлами
+      {
+        // test - какие файлы должен обрабатывать лоадер
+        test: /\.tsx?$/,
+        // use - название лоадера
+        use: "ts-loader",
+        // test - какие файлы должен игнорировать лоадер
+        exclude: /node_modules/,
+      },
+      // лоадер, который учит работать с .css файлами
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      { test: /\.(js)$/, use: "babel-loader" },
+    ],
+  },
+
+
+  // плагины – это дополнительная логика для нашего проекта
+  plugins: [
+    // HtmlWebpackPlugin позволяет не просто собирать все файлы в один, но и вставлять этот единственный файл в html в тег <script>
+    new HtmlWebpackPlugin({ template: './public/index.html'}),
+    // CleanWebpackPlugin - при новой сборке очищает папки от мусора, оставшегося с предыдущих сборок
+    new CleanWebpackPlugin(),
+  ],
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer/"),
+      util: require.resolve("util/"),
+    },
+  },
+
+  devServer: {
+    historyApiFallback: true,
+  },
+};
